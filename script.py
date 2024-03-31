f49ba99e1b805a6e857c373ae5ea80ad4aa496d7
Ubuntu <ubuntu@ip-172-31-46-31.eu-north-1.compute.internal>
2024-03-31 16:53:38 +0000

    script commit

---
+++ script.py
@@ -0,0 +1,9 @@
+[200~# Example Python script
+
+        def greet(name):
+            print("Hello, {}!".format(name))
+
+            if __name__ == "__main__":
+                name = input("Enter your name: ")
+                    greet(name)
+
